ec54bf0c497110e15c9335ee7aaa5da52d5e30bd
tanno-maker <t.tanno0827@gmail.com>
2022-08-24 19:05:41 +0900

    VisualScriptingのテスト
    Assets/Scenes/TestScenes/VisualScriptingTest.unity にテストシーンあり。

---
+++ Unity/ReaperRobot/Assets/Scipts/ReaperGame/VisualScriptingTest/VSReaperController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.InputSystem;
+
+namespace smart3tene.Reaper
+{
+    public class VSReaperController : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            float horizontal = 0f;
+            float vertical = 0f;
+            if (Keyboard.current.wKey.IsPressed())
+            {
+                vertical += 1f;
+            }
+            if(Keyboard.current.sKey.IsPressed())
+            {
+                vertical -= 1f;
+            }
+            if (Keyboard.current.aKey.IsPressed())
+            {
+                horizontal -= 1f;
+            }
+            if (Keyboard.current.dKey.IsPressed())
+            {
+                horizontal += 1f;
+            }
+
+            CustomEvent.Trigger(gameObject, "TestEvent", horizontal, vertical);
+        }
+    }
+
+}
