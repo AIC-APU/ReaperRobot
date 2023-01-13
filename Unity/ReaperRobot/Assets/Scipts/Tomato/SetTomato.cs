256f390ea2331fe5cafa775d785409ef8048da4b
tanno-maker <t.tanno0827@gmail.com>
2023-01-13 11:05:07 +0900

    トマト・トマト木プレハブの作成。トマトを動的配置するクラス SetTomato.cs を作成。

--- Unity/ReaperRobot/Assets/Scipts/Tomato/SetTomato.cs
+++ Unity/ReaperRobot/Assets/Scipts/Tomato/SetTomato.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms.VisualStyles;
 using UnityEngine;
 
 namespace smart3tene
@@ -11,30 +12,50 @@ namespace smart3tene
         #region Serialized Private Fields
         [SerializeField] private List<Transform> _tomatoLocations = new List<Transform>();
         [SerializeField] private List<GameObject> _tomatoPrefabs = new List<GameObject>();
-        [SerializeField, Range(0, 10)] private int _tomatoNum = 5;
         #endregion
 
         #region MonoBehaviour Callbacks
         private void Awake()
         {
-            RandomSetTomato(_tomatoNum);
+            var tomatoNum = DecideTomatoNum(_tomatoLocations.Count);
+
+            RandomSetTomato(tomatoNum);
         }
         #endregion
 
         #region Private method
+        private int DecideTomatoNum(int maxNum)
+        {
+            var random = new System.Random();
+            return random.Next(0, maxNum + 1);
+
+            //今はランダムに決めているが、
+            //将来的には木の成長具合や時期などから、トマトの数を決定したい
+            //例えば未熟な木ならトマトを少なくしたり、夏なら多くしたりなど
+        }
+
         private void RandomSetTomato(int tomatoNum)
         {
+            //トマトを実らせる順番をランダムにしているが、実際は実る法則などあるのだろうか...?
             var randomLocList = _tomatoLocations
                                 .OrderBy(x => Guid.NewGuid())
                                 .ToList();
 
-            var random = new System.Random();
-
             for(int i = 0; i < tomatoNum; i++)
             {
-                var tomatoIndex = random.Next(0, _tomatoPrefabs.Count);
+                var tomatoIndex = DecideTomatoIndex(_tomatoPrefabs.Count);
                 Instantiate(_tomatoPrefabs[tomatoIndex], randomLocList[i]);
-            }
+            }            
+        }
+
+        private int DecideTomatoIndex(int prefabCount)
+        {
+            var random = new System.Random();
+            return random.Next(0, prefabCount);
+
+            //今はランダムに決めているが、
+            //将来的には木の成長具合や時期などから、生るトマトに偏りを持たせたい
+            //例えば未熟な木なら青いトマトを多くしたり、夏なら赤いトマトを多くしたりなど
         }
         #endregion
     }
