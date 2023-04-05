4f084ada39319778af3d1561612730821151856d
tanno-maker <t.tanno0827@gmail.com>
2023-04-05 16:58:11 +0900

    MenuOpenerの改修

--- Unity/ReaperRobot/Assets/Scripts/_UnityComponent/Menu/MenuOpner.cs
+++ Unity/ReaperRobot/Assets/Scripts/_UnityComponent/Menu/MenuOpner.cs
@@ -1,74 +1,85 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UniRx;
 
 namespace ReaperRobot.Scripts.UnityComponent.Menu
 {
     [RequireComponent(typeof(PlayerInput))]
     public class MenuOpner : MonoBehaviour
     {
+        #region Serialized Private Fields
+        [SerializeField] private GameObject _menuCanvas;
+
+        [Header("When Menu is Open...")]
+        [SerializeField] private bool _pauseGame = true;
+        [SerializeField] private bool _disableController = true;
+        #endregion
+
         #region Private Fields
         private PlayerInput _playerInput;
         private InputActionMap _menuMap;
-        private bool _isMenuOpen = false;
         #endregion
 
         #region MonoBehaviour Callbacks
         void Awake()
         {
+            //PlayerInputの初期設定
             _playerInput = GetComponent<PlayerInput>();
             _menuMap = _playerInput.actions.FindActionMap("Menu");
             _menuMap.Enable();
 
+            //MenuCanvasを非表示にする
+            _menuCanvas.SetActive(false);
+
+            //MenuCanvasの表示状態に合わせて処理を行う
+            _menuCanvas
+                .ObserveEveryValueChanged(x => x.activeSelf)
+                .Skip(1)
+                .Subscribe(isOpen =>
+                {
+                    if (isOpen)
+                    {
+                        if (_pauseGame)
+                        {
+                            Time.timeScale = 0;
+                        }
+                        if (_disableController)
+                        {
+                            _playerInput.enabled = false;
+                        }
+                    }
+                    else
+                    {
+                        if (_pauseGame)
+                        {
+                            Time.timeScale = 1;
+                        }
+                        if (_disableController)
+                        {
+                            _playerInput.enabled = true;
+                        }
+                    }
+                })
+                .AddTo(this);
+
+            //イベント登録
             _menuMap["Open"].started += MenuAction;
         }
 
         private void OnDestroy()
-        {
+        {   
+            //イベント解除
             _menuMap["Open"].started -= MenuAction;
-            Time.timeScale = 1;
+
+            //TimerScaleを元に戻す
+            if(_pauseGame) Time.timeScale = 1;
         }
         #endregion
 
         #region Private method
         private void MenuAction(InputAction.CallbackContext obj)
         {
-            if (_isMenuOpen)
-            {
-            }
-            else
-            {
-
-            }
-        }
-
-        private void Open()
-        {
-            //メニューを開いたというフラグを立てる
-            _isMenuOpen = true;
-
-            //コントローラ操作をできないようにする
-            _playerInput.enabled = false;
-
-            //タイマーが動いていたらタイマーを止める
-
-
-            //ゲームを停止
-            Time.timeScale = 0;
-        }
-
-        private void Close()
-        {
-            //メニューを開いたというフラグを立てる
-            _isMenuOpen = false;
-
-            //コントローラ操作を可能に
-            _playerInput.enabled = true;
-
-            //タイマーを再び起動
-
-
-            //ゲームを停止
-            Time.timeScale = 1;
+            _menuCanvas.SetActive(!_menuCanvas.activeSelf);
         }
         #endregion
     }
