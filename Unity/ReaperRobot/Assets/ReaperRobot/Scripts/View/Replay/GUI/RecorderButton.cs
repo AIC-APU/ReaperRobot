ac4e8dfe02e3c13736ece042d758e8b01f13d3f1
tanno-maker <t.tanno0827@gmail.com>
2023-05-10 18:23:08 +0900

    再生・記録シーンのとりあえず完成。あとロボット移動時のコントローラの入力値を変更

---
+++ Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Replay/GUI/RecorderButton.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using Plusplus.ReaperRobot.Scripts.View.ReaperRobot;
+
+namespace Plusplus.ReaperRobot.Scripts.View.Replay
+{
+    public class RecorderButton : MonoBehaviour
+    {
+        #region Serialized Private Fields
+        [SerializeField] private Recorder _recorder;
+        [SerializeField] private ReaperController _reaperController;
+        [SerializeField] private Button _recordButton;
+        [SerializeField] private Button _stopButton;
+        [SerializeField] private Button _backButton;
+        #endregion
+
+        #region Private Fields
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+        void Awake()
+        {
+            _recordButton.onClick.AddListener(PlayButton);
+            _stopButton.onClick.AddListener(StopButton);
+            _backButton.onClick.AddListener(BackButton);
+
+            SetIntaractable(true, false, false);
+
+            _reaperController.enabled = false;
+        }
+        #endregion
+
+        #region Private method
+        private void PlayButton()
+        {
+            _recorder.StartRecording();
+            SetIntaractable(false, true, true);
+            _reaperController.enabled = true;
+        }
+
+        private async void StopButton()
+        {
+            _recorder.StopRecording();
+            _reaperController.enabled = false;
+            SetIntaractable(false, false, false);
+
+            //書き込みが終わるまで1秒待つ
+            //本当はちゃんと書き込みが終わったか確認するべき
+            //至急必要ないので後回し
+            await UniTask.Delay(1000);
+
+            SetIntaractable(false, false, true);
+        }
+
+        private void BackButton()
+        {
+            _recorder.ResetRecording();
+            SetIntaractable(true, false, false);
+            _reaperController.enabled = false;
+        }
+
+        private void SetIntaractable(bool play, bool stop, bool back)
+        {
+            _recordButton.interactable = play;
+            _stopButton.interactable = stop;
+            _backButton.interactable = back;
+        }
+        #endregion
+    }
+}
---
+++ Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Replay/Replay/ShadowLiftReplay.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UniRx;
+using Plusplus.ReaperRobot.Scripts.View.ReaperRobot;
+
+namespace Plusplus.ReaperRobot.Scripts.View.Replay
+{
+    public class ShadowLiftReplay : BaseReplay
+    {
+        #region Serialized Private Fields
+        [SerializeField] private ShadowReaperManager _shadowManager;
+        #endregion
+        
+        #region MonoBehaviour Callbacks
+        void Awake()
+        {
+            _replayManager
+                .Time
+                .Where(_ => _replayManager.IsDataReady)
+                .Subscribe(_ =>
+                {
+                    Replay();
+                })
+                .AddTo(this);
+        }
+        #endregion
+
+        #region Private method
+        protected override void Replay()
+        {
+            var liftData = _replayManager.GetLift();
+            _shadowManager.RotateCutter(liftData);
+        }
+        #endregion
+    }
+}
