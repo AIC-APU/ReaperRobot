5a114df60b0027d863269d75f573ef98e1ea42fe
tanno-maker <t.tanno0827@gmail.com>
2023-09-06 15:07:09 +0900

    草刈りシーンでのおよねの配置とNavAgentAnimation.csの微修正

--- Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Person/NPC/NavAgentAnimation.cs
+++ Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Person/NPC/NavAgentAnimation.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UniRx;
 using Cysharp.Threading.Tasks;
 
 namespace Plusplus.ReaperRobot.Scripts.View.Person
@@ -28,19 +25,31 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
             //移動アニメーション
             var speed = new Vector2(_agent.velocity.x, _agent.velocity.z).magnitude;
             _animator.SetFloat("Speed", speed * _speedRate, 0.05f, Time.deltaTime);
+
+
+            //目標地点に到達したら待機アニメーション(未実装)
+            // if (_agent.hasPath && _agent.remainingDistance < _agent.stoppingDistance)
+            // {
+            //     _animator.SetBool("hoge_motion", true);
+            // }
+            // else
+            // {
+            //     _animator.SetBool("hoge_motion", false);
+            // }
         }
 
         async void OnCollisionEnter(Collision collision)
         {
             if(_animator.GetAnimatorTransitionInfo(0).IsName("Base Layer -> Collision")) return;
             
+            //衝突対象に衝突したら一時停止
             if (collision.gameObject == _collisionTarget 
                 || IsChildOfTarget(collision.gameObject, _collisionTarget))
             {
                 _agent.speed = 0;
                 _animator.SetTrigger("Collision");
 
-                await UniTask.Delay(1000);
+                await UniTask.Delay(2000);
 
                 _agent.speed = 1;
             }
@@ -61,6 +70,5 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
                 return IsChildOfTarget(child.transform.parent.gameObject, parent);
             }
         }
-
     }
 }
