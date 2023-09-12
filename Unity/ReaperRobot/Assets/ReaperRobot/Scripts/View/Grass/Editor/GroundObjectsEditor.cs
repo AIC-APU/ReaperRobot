306ce9a0f796c77b05d7b08035b54448e6de64ce
tanno-maker <t.tanno0827@gmail.com>
2023-09-12 15:36:28 +0900

    新草刈りフィールドに草を配置

---
+++ Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Grass/Editor/GroundObjectsEditor.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Plusplus.ReaperRobot.Scripts.View.Grass
+{
+    [CustomEditor(typeof(GrassObject))]
+    [CanEditMultipleObjects]
+    public class GroundObjectsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            GUILayout.Space(10f);
+
+            if (GUILayout.Button("Ground Objects"))
+            {
+                GroundSelectedObjects();
+            }
+        }
+
+        private void GroundSelectedObjects()
+        {
+            // 選択されたオブジェクトのリストを取得
+            GameObject[] selectedObjects = Selection.gameObjects;
+
+            foreach (GameObject obj in selectedObjects)
+            {
+                // オブジェクトの位置を取得
+                Vector3 objectPosition = obj.transform.position;
+
+                // レイキャストを使用して地面に接地させる
+                RaycastHit hit;
+                if (Physics.Raycast(objectPosition, Vector3.down, out hit, 10f))
+                {
+                    float distanceToGround = hit.distance;
+                    objectPosition.y -= distanceToGround;
+                    obj.transform.position = hit.point;
+
+                    // オブジェクトを地面上に垂直に配置
+                    Vector3 objectUpDirection = obj.transform.up;
+                    Quaternion rotation = Quaternion.FromToRotation(objectUpDirection, hit.normal);
+                    obj.transform.rotation = rotation * obj.transform.rotation;
+                }
+            }
+        }
+    }
+}
