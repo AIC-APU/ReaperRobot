936050b36516ef084cd19c4868bb67c6418f68d1
tanno-maker <t.tanno0827@gmail.com>
2023-11-13 16:55:11 +0900

    NPCに木での作業アニメーションを追加

--- Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Person/NPC/NavAgentAnimation.cs
+++ Unity/ReaperRobot/Assets/ReaperRobot/Scripts/View/Person/NPC/NavAgentAnimation.cs
@@ -10,10 +10,14 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
     public class NavAgentAnimation : MonoBehaviour
     {
         [SerializeField] private GameObject _collisionTarget;
-        [SerializeField] private float _speedRate = 0.8f;
+        [SerializeField] private float _speedAnimationRate = 0.8f;
+        [SerializeField] private float _angularAnimationRate = 0.001f;
 
         private Animator _animator;
         private NavMeshAgent _agent;
+
+        private float _defaultSpeed;
+        private float _defaultAngularSpeed;
 
         void Awake()
         {
@@ -27,18 +31,19 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
         {
             //移動アニメーション
             var speed = new Vector2(_agent.velocity.x, _agent.velocity.z).magnitude;
-            _animator.SetFloat("Speed", speed * _speedRate, 0.05f, Time.deltaTime);
+            var angureSpeed = _agent.angularSpeed;
+            _animator.SetFloat("Speed", speed * _speedAnimationRate + angureSpeed * _angularAnimationRate, 0.05f, Time.deltaTime);
 
 
-            //目標地点に到達したら待機アニメーション(未実装)
-            // if (_agent.hasPath && _agent.remainingDistance < _agent.stoppingDistance)
-            // {
-            //     _animator.SetBool("hoge_motion", true);
-            // }
-            // else
-            // {
-            //     _animator.SetBool("hoge_motion", false);
-            // }
+            //目標地点に到達したら待機アニメーション
+            if (_agent.remainingDistance < _agent.stoppingDistance)
+            {
+                _animator.SetBool("TreeCare", true);
+            }
+            else
+            {
+                _animator.SetBool("TreeCare", false);
+            }
         }
 
         void OnCollisionEnter(Collision collision)
@@ -49,7 +54,11 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
             if (collision.gameObject == _collisionTarget
                 || IsChildOfTarget(collision.gameObject, _collisionTarget))
             {
+                _defaultSpeed = _agent.speed;
+                _defaultAngularSpeed = _agent.angularSpeed;
+
                 _agent.speed = 0;
+                _agent.angularSpeed = 0;
                 _animator.SetTrigger("Collision");
             }
         }
@@ -60,6 +69,7 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
                 || IsChildOfTarget(collision.gameObject, _collisionTarget))
             {
                 _agent.speed = 0;
+                _agent.angularSpeed = 0;
             }
         }
 
@@ -69,7 +79,8 @@ namespace Plusplus.ReaperRobot.Scripts.View.Person
                 || IsChildOfTarget(collision.gameObject, _collisionTarget))
             {
                 await UniTask.Delay(3000);
-                _agent.speed = 1f;
+                _agent.speed = _defaultSpeed;
+                _agent.angularSpeed = _defaultAngularSpeed;
             }
         }
 
